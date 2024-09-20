1549f9854b35549c0ccfd1b3f96ab9b1443649c5
bodyangug <aseevbogdan5@gmail.com>
2024-09-20 16:01:04 +0300

    chore: change todo

--- src/main/kotlin/com/pandus/llm/rag/RagKotlinApplication.kt
+++ src/main/kotlin/com/pandus/llm/rag/RagKotlinApplication.kt
@@ -10,11 +10,9 @@ import kotlinx.serialization.json.Json
 import org.koin.ktor.plugin.Koin
 
 // TODO:
-//  1. add answering by context. Now we get 429 too many requests error;
-//  2. add HuggingFace api for loading new models;
-//  3. Launch Chroma locally;
-//  4. add possibility to generate imag;
-//  5. add auth.
+//  1. Launch Chroma locally;
+//  2. Add possibility to generate image;
+//  3. Add auth.
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
