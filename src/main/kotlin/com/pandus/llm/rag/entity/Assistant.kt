23a4e4b0d06cdce911e6b2330624ecf728b207d3
bodyangug <aseevbogdan5@gmail.com>
2024-09-23 17:12:52 +0300

    feat: change system message

--- src/main/kotlin/com/pandus/llm/rag/entity/Assistant.kt
+++ src/main/kotlin/com/pandus/llm/rag/entity/Assistant.kt
@@ -5,6 +5,6 @@ import dev.langchain4j.service.SystemMessage
 import dev.langchain4j.service.UserMessage
 
 interface Assistant {
-    @SystemMessage("You are code friend, answer me using programming slang.")
+    @SystemMessage("You are friend, answer me using slang.")
     fun chat(@UserMessage message: String, @MemoryId chatId: String): String
 }
